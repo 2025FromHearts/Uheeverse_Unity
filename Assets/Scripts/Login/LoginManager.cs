e36d61e8032cee3e8a6627f0d59c1920e3a2bc70
Yeonwoo <133082158+lxr4817@users.noreply.github.com>
2025-05-30 12:45:09 +0900

    Inventory System
    인벤토리 시스템 구현

--- Assets/Scripts/Login/LoginManager.cs
+++ Assets/Scripts/Login/LoginManager.cs
@@ -2,19 +2,33 @@
 using UnityEngine.Networking;
 using System.Collections;
 using TMPro;
+using Newtonsoft.Json;
 
 public class LoginManager : MonoBehaviour
 {
     public TMP_InputField usernameInput;
     public TMP_InputField passwordInput;
     private string accessToken;
+    private string characterId;
 
-    // JWT 응답 파싱용 구조
     [System.Serializable]
+    public class LoginRequest
+    {
+        public string username;
+        public string password;
+    }
+
     public class LoginResponse
     {
         public string access;
         public string refresh;
+    }
+
+    public class UserInfo
+    {
+        public string user_id;
+        public string username;
+        public string character_id;
     }
 
     public void OnLoginButtonClick()
@@ -26,14 +40,13 @@ public class LoginManager : MonoBehaviour
 
     IEnumerator Login(string username, string password)
     {
-        // 로그인 요청 JSON 데이터 생성
         LoginRequest requestData = new LoginRequest
         {
             username = username,
             password = password
         };
 
-        string jsonData = JsonUtility.ToJson(requestData);
+        string jsonData = JsonConvert.SerializeObject(requestData);
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
 
         UnityWebRequest www = new UnityWebRequest("http://localhost:8000/users/token/", "POST");
@@ -46,19 +59,19 @@ public class LoginManager : MonoBehaviour
         if (www.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError("Login failed: " + www.error);
-            Debug.Log("Sending JSON: " + jsonData);
         }
         else
         {
-            // JWT 토큰 파싱 및 저장
-            LoginResponse res = JsonUtility.FromJson<LoginResponse>(www.downloadHandler.text);
+            LoginResponse res = JsonConvert.DeserializeObject<LoginResponse>(www.downloadHandler.text);
             accessToken = res.access;
 
-            // 토큰 저장 추가
-            PlayerPrefs.SetString("access_token", res.access);
+            PlayerPrefs.SetString("access_token", accessToken);
             PlayerPrefs.Save();
             Debug.Log("Login success! Access Token: " + accessToken);
-            StartCoroutine(GetUserInfo());
+
+            yield return StartCoroutine(GetUserInfo());
+            yield return StartCoroutine(CheckOrCreateInventory());
+
             UnityEngine.SceneManagement.SceneManager.LoadScene("MyStation");
         }
     }
@@ -76,7 +89,40 @@ public class LoginManager : MonoBehaviour
         }
         else
         {
-            Debug.Log("User info: " + www.downloadHandler.text);
+            string json = www.downloadHandler.text;
+            Debug.Log("User info raw JSON: " + json);
+
+            try
+            {
+                UserInfo userInfo = JsonConvert.DeserializeObject<UserInfo>(json);
+                characterId = userInfo.character_id;
+                Debug.Log("Parsed characterId: " + characterId);
+                PlayerPrefs.SetString("character_id", characterId);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError("Failed to parse user info: " + e.Message);
+            }
+        }
+    }
+
+    IEnumerator CheckOrCreateInventory()
+    {
+        string url = "http://127.0.0.1:8000/item/inventory/init/" + characterId + "/";
+        Debug.Log("Calling URL: " + url);
+
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        www.SetRequestHeader("Authorization", "Bearer " + accessToken);
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Inventory init failed: " + www.error);
+        }
+        else
+        {
+            Debug.Log("Inventory checked or created: " + www.downloadHandler.text);
         }
     }
 }
