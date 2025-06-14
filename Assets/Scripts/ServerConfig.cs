96ade2619e20606d201d126b9b6d0fe170c095ef
Sennin <semin4599@naver.com>
2025-06-15 02:11:00 +0900

    0615-1

--- Assets/Scripts/ServerConfig.cs
+++ Assets/Scripts/ServerConfig.cs
@@ -1,4 +1,4 @@
 public static class ServerConfig
 {
-    public static string baseUrl = "https://209f-203-252-223-254.ngrok-free.app"; // ngrok �ּ� ���⿡ �Է�
+    public static string baseUrl = "https://970d-203-252-223-254.ngrok-free.app"; // ngrok �ּ� ���⿡ �Է�
 }
