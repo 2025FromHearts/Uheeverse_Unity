64588ab21961e782fa0ff5972a1733b2796d5eda
Juhyeon <dhwngus@duksung.ac.kr>
2025-08-05 12:51:11 +0900

    Mystation_Modeling_1

--- Assets/Scripts/ServerConfig.cs
+++ Assets/Scripts/ServerConfig.cs
@@ -1,4 +1,5 @@
 public static class ServerConfig
 {
-    public static string baseUrl = "https://970d-203-252-223-254.ngrok-free.app"; // ngrok 주소 여기에 입력
+    //public static string baseUrl = "https://970d-203-252-223-254.ngrok-free.app"; // ngrok 주소 여기에 입력
+    public static string baseUrl = "https://c77d71444e12.ngrok-free.app";
 }
