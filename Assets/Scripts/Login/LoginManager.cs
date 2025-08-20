8aaf8e5772a973db47a460e77ff654c5c3b14da7
Sennin <semin4599@naver.com>
2025-08-21 01:31:49 +0900

    LoadingMystation

--- Assets/Scripts/Login/LoginManager.cs
+++ Assets/Scripts/Login/LoginManager.cs
@@ -3,9 +3,18 @@ using UnityEngine.Networking;
 using System.Collections;
 using TMPro;
 using Newtonsoft.Json;
+using FishNet;
+using FishNet.Transporting;
+using System;
+using FishNet.Object;
+using FishNet.Connection;
+using UnityEngine.SceneManagement;
+
+using FishNet.Managing.Scened;
 
 public class LoginManager : MonoBehaviour
 {
+    private SceneLoadingManager slm;
     public TMP_InputField usernameInput;
     public TMP_InputField passwordInput;
     private string accessToken;
@@ -33,13 +42,26 @@ public class LoginManager : MonoBehaviour
         public string character_id;
     }
 
+    private void Awake()
+    {
+        Debug.Log("LoginManager Awake 실행됨");
+
+
+
+    }
+
+
+
+    [Obsolete]
     public void OnLoginButtonClick()
     {
         string username = usernameInput.text;
         string password = passwordInput.text;
         StartCoroutine(Login(username, password));
+        Debug.Log("로그인버튼실행됨");
     }
 
+    [Obsolete]
     IEnumerator Login(string username, string password)
     {
         LoginRequest requestData = new LoginRequest
@@ -56,12 +78,16 @@ public class LoginManager : MonoBehaviour
         www.downloadHandler = new DownloadHandlerBuffer();
         www.SetRequestHeader("Content-Type", "application/json");
 
+        Debug.Log("로그인코루틴");
+
         yield return www.SendWebRequest();
 
         if (www.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError("Login failed: " + www.error + "\n응답: " + www.downloadHandler.text);
             Debug.Log("Sending JSON: " + jsonData);
+
+            Debug.Log("로그인응답성공");
         }
         else
         {
@@ -74,59 +100,69 @@ public class LoginManager : MonoBehaviour
             Debug.Log("Login success! Access Token: " + accessToken);
 
             yield return StartCoroutine(GetUserInfo());
-            yield return StartCoroutine(CheckOrCreateInventory());
 
-            UnityEngine.SceneManagement.SceneManager.LoadScene("MyStation");
+            Debug.Log("유저 받아오기");
+
+            slm = SceneLoadingManager.Instance;
+            slm.CreateSessionFromTagServerRpc(SceneType.Station, "StartScene");
+
+
         }
-    }
 
-    IEnumerator GetUserInfo()
-    {
-        UnityWebRequest www = UnityWebRequest.Get(ServerConfig.baseUrl + "/users/get_user_info/");
-        www.SetRequestHeader("Authorization", "Bearer " + accessToken);
-
-        yield return www.SendWebRequest();
-
-        if (www.result != UnityWebRequest.Result.Success)
+        IEnumerator GetUserInfo()
         {
-            Debug.LogError("User info request failed: " + www.error);
-        }
-        else
-        {
-            string json = www.downloadHandler.text;
-            Debug.Log("User info raw JSON: " + json);
+            string token = PlayerPrefs.GetString("access_token");
+            UnityWebRequest www = UnityWebRequest.Get(ServerConfig.baseUrl + "/users/get_user_info/");
+            www.SetRequestHeader("Authorization", "Bearer " + token);
 
-            try
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
             {
-                UserInfo userInfo = JsonConvert.DeserializeObject<UserInfo>(json);
-                characterId = userInfo.character_id;
-                Debug.Log("Parsed characterId: " + characterId);
-                PlayerPrefs.SetString("character_id", characterId);
+                Debug.LogError("User info request failed: " + www.error);
             }
-            catch (System.Exception e)
+            else
             {
-                Debug.LogError("Failed to parse user info: " + e.Message);
+                string json = www.downloadHandler.text;
+                Debug.Log("User info raw JSON: " + json);
+
+                try
+                {
+                    UserInfo userInfo = JsonConvert.DeserializeObject<UserInfo>(json);
+                    characterId = userInfo.character_id;
+                    Debug.Log("Parsed characterId: " + characterId);
+                    PlayerPrefs.SetString("character_id", characterId);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError("Failed to parse user info: " + e.Message);
+                }
             }
         }
-    }
 
-    IEnumerator CheckOrCreateInventory()
-    {
-        string url = ServerConfig.baseUrl + "/item/inventory/init/" + characterId + "/";
-        Debug.Log("Calling URL: " + url);
-
-        UnityWebRequest www = UnityWebRequest.Get(url);
-        www.SetRequestHeader("Authorization", "Bearer " + accessToken);
-
-        yield return www.SendWebRequest();
-
-        if (www.result != UnityWebRequest.Result.Success)
+        IEnumerator CheckOrCreateInventory()
         {
-            Debug.LogError("Inventory init failed: " + www.error);
+            string url = ServerConfig.baseUrl + "/item/inventory/init/" + characterId + "/";
+            Debug.Log("Calling URL: " + url);
+
+            string token = PlayerPrefs.GetString("access_token");
+
+            UnityWebRequest www = UnityWebRequest.Get(url);
+            www.SetRequestHeader("Authorization", "Bearer " + token);
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Inventory init failed: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Inventory checked or created: " + www.downloadHandler.text);
+            }
         }
-        else
-        {
-            Debug.Log("Inventory checked or created: " + www.downloadHandler.text);
-        }
+
+
     }
+    
 }
