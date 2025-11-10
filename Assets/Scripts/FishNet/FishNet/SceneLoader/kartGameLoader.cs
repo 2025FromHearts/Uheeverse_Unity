b0d269854f2f533c629eafcf4bbaef1cbf6b86b6
Sennin <semin4599@naver.com>
2025-11-11 02:55:47 +0900

    빌드 땅 뚫림 해결

--- Assets/Scripts/FishNet/FishNet/SceneLoader/kartGameLoader.cs
+++ Assets/Scripts/FishNet/FishNet/SceneLoader/kartGameLoader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Connection;
 using FishNet.Object;
-using NiceIO.Sysroot;
 public class kartGameLoader : NetworkBehaviour
 {
     public SceneLoadingManager slm;
